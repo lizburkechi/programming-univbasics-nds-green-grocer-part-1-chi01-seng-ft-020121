522d2d4484221a3b8540fb0b3770b65fe091827d
Liz Burke <liz.burke.chi@gmail.com>
2021-01-27 23:58:49 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,7 @@ def consolidate_cart(cart)
   new_cart = []
   counter = 0
   while counter < cart.length
-       expect(consolidated_cart[i][:count]).to eq(1)
-       i += 1
+       new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+       counter += 1
      end
 end
