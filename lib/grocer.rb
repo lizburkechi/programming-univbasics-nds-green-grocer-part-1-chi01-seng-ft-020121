18cf46c5aeb98a02e3cfe0e36edb1af5ae7aae45
Liz Burke <liz.burke.chi@gmail.com>
2021-01-27 23:49:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,12 @@
 def find_item_by_name_in_collection(name, collection)
 counter = 0
 while counter < collection.length
+  if collection[counter][:item] == name
+    return collection[counter]  
+  end
   collection[counter]
   counter += 1
-end
+  end
 end
 
 def consolidate_cart(cart)
